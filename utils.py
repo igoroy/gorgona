4e207a8f46730135253beeb1e40ab454d164f763
Maxim Sosedov <sosedow@gmail.com>
2015-01-08 16:54:30 +0200

    MikroTik parse util

--- utils.py
+++ utils.py
@@ -2,3 +2,6 @@ def cisco_to_ieee_802(cisco_mac):
     digits = cisco_mac.upper().replace('.','')
     ieee_mac = ':'.join([digits[0+2*i:2+2*i] for i in range(0,6)])
     return ieee_mac
+
+def parse_routeOS(host, login, passwrd):
+    pass
